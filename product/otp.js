3735c04be96a5e88cd4c4ad280d5efb47a5b1e12
NirmalGehlot <48903097+NirmalGehlot@users.noreply.github.com>
2019-04-04 12:29:42 +0530

    Delete otp.js

--- product/otp.js
+++
@@ -1,9 +0,0 @@
-
-const otplib= require('otplib');
-
-module.exports.otp = function(){
-
-  const secret = otplib.authenticator.generateSecret();
-  const token = otplib.authenticator.generate(secret);
-  return token;
-}
